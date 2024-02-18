86e621ed7db7a4bfe615f92de2a52b2def0fd016
Rohithhowk <rohith28chowki@gmail.com>
2024-02-18 15:02:16 +0530

    first commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000", // Replace with your backend server URL
+      target: "http://13.201.93.229:5000", // Replace with your backend server URL
       changeOrigin: true,
     })
   );
